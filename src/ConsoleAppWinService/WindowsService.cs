b9a9bf4850f50854b326cc3438a6143b73e2eabe
Arvydas Grigonis <arnvidhr@users.noreply.github.com>
2023-08-24 01:18:09 +0300

    Initial update

---
+++ src/ConsoleAppWinService/WindowsService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWinService
+{
+	partial class WindowsService : ServiceBase
+	{
+		#region Attributes
+
+		#endregion
+
+		#region Properties
+
+		#endregion
+
+		#region Ctor
+
+		public WindowsService()
+		{
+			InitializeComponent();
+		}
+
+		#endregion
+
+		#region Methods
+		internal void Start(string[] args)
+		{
+			OnStart(args);
+		}
+
+		protected override void OnStart(string[] args)
+		{
+		}
+
+		protected override void OnStop()
+		{
+		}
+
+		#endregion
+	}
+}
